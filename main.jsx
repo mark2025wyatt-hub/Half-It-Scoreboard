48b9b38775df4a9abae7f353137a173110edb58b
mark2025wyatt-hub <mark2025wyatt@gmail.com>
2026-08-13 16:22:43 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HalfItScoreboard from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HalfItScoreboard />
-  </React.StrictMode>
-);
